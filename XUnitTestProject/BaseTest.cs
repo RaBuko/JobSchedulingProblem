d8c465be6b39c476229380608d2b8faf4885623d
RaBuko <raf.buko@gmail.com>
2020-05-01 11:00:58 +0200

    base test

---
+++ XUnitTestProject/BaseTest.cs
@@ -0,0 +1,83 @@
+﻿using FormsApp.Helpers;
+using Solver.Data;
+using Solver.Methods;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Xunit.Abstractions;
+
+namespace XUnitTestProject
+{
+    public abstract class BaseTest
+    {
+        private readonly ITestOutputHelper output;
+        protected readonly List<Job> data;
+        private int bestTardiness = int.MaxValue;
+        private readonly string file;
+        private readonly int jobsCount;
+        private readonly int testsToMake;
+
+        protected BaseTest(ITestOutputHelper outputHelper, int inJobsCount, int inTestsToMake)
+        {
+            testsToMake = inTestsToMake;
+            jobsCount = inJobsCount;
+            output = outputHelper;
+            file = $"{Helper.AppSettings.ExamplesPath}\\{jobsCount}.txt";
+            if (!File.Exists(file))
+            {
+                data = Generator.GenerateJobs(new GeneratorOptions()
+                {
+                    JobsCount = jobsCount,
+                    MaxTerm = jobsCount * 2,
+                    MaxTime = jobsCount * 2,
+                    MaxWeight = jobsCount * 2,
+                });
+                Loader.WriteJobsToFile(file, data);
+            }
+
+            var found = Loader.FindBest(Path.GetFileNameWithoutExtension(file), Helper.AppSettings.SolvedDictFileName);
+            if (found.Contains("-")) bestTardiness = int.MaxValue;
+            else bestTardiness = int.Parse(found);
+            data = Loader.LoadJobsFromFile(file);
+        }
+
+        protected void TestMethod(IMethod method, IMethodOptions options)
+        {
+            var results = new List<Result>();
+            output.WriteLine("START Prepare");
+            var stopwatch = new Stopwatch();
+            options = method.Prepare(options);
+            stopwatch.Stop();
+            string elapsedPrepare = stopwatch.Elapsed.ToString("G");
+            output.WriteLine("END Prepare");
+
+            for (int i = 0; i < testsToMake + 1; i++)
+            {
+                stopwatch = new Stopwatch();
+                var (jobOrder, minTardiness) = method.Solve(options, stopwatch);
+                stopwatch.Stop();
+                var result = new Result()
+                {
+                    Score = minTardiness,
+                    ExecElapsed = stopwatch.Elapsed.ToString("G"),
+                    PrepareElapsed = elapsedPrepare,
+                };
+                if (i != 0) results.Add(result);
+                Helper.LogStatus($"{method.GetType().Name} {i}/{testsToMake} | ExecElapsed = {result.ExecElapsed}");
+            }
+
+            var min = results.Min(x => x.Score);
+            if (bestTardiness > min) { bestTardiness = min; Loader.TrySaveNewBest(file, bestTardiness, Helper.AppSettings.SolvedDictFileName); }
+            string expandedName = "";
+            if (method.GetType() == typeof(GeneticAlgorithmMethod))
+            {
+                var genOptions = options as GeneticAlgorithmOptions;
+                expandedName = $"_CC{genOptions.ChromosomeCount}_CR{genOptions.CrossoverRate:0%}_MR{genOptions.MutationRate:0%}_I{genOptions.NumberOfIterations.ToString()}_{string.Join("", genOptions.SelectionMechanism.ToString().Where(x => char.IsUpper(x)))}";
+            }
+            Helper.WriteResultsToCsv(method.GetType().Name, jobsCount, bestTardiness, results);
+        }
+    }
+}
--- XUnitTestProject/Test10.cs
+++ XUnitTestProject/Test10.cs
@@ -1,120 +1,24 @@
-using FormsApp.Helpers;
-using Solver.Data;
 using Solver.Methods;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace XUnitTestProject
 {
-    public class Test10
+    public class Test10 : BaseTest
     {
-        private readonly ITestOutputHelper output;
-        private readonly int jobsCount = 10;
-        private readonly int numOfTests = 10;
-        private readonly List<Job> data;
-        private int bestTardiness = int.MaxValue;
-        private readonly string file;
-
-        public Test10(ITestOutputHelper outputHelper)
-        {
-            output = outputHelper;
-            file = $"{Helper.AppSettings.ExamplesPath}\\{jobsCount}.txt";
-            if (!File.Exists(file))
-            {
-                data = Generator.GenerateJobs(new GeneratorOptions()
-                {
-                    JobsCount = jobsCount,
-                    MaxTerm = jobsCount * 2,
-                    MaxTime = jobsCount * 2,
-                    MaxWeight = jobsCount * 2,
-                });
-                Loader.WriteJobsToFile(file, data);
-            }
-
-            var found = Loader.FindBest(Path.GetFileNameWithoutExtension(file), Helper.AppSettings.SolvedDictFileName);
-            if (found.Contains("-")) bestTardiness = int.MaxValue;
-            else bestTardiness = int.Parse(found);
-            data = Loader.LoadJobsFromFile(file);
-        }
+        public Test10(ITestOutputHelper outputHelper) : base(outputHelper, 10, 10) { }        
 
         [Fact]
-        public void BruteForceTest()
-        {
-            var results = new List<Result>();
-            var options = new BruteForceOptions() { Data = data, };
-            var method = new BruteForceMethod();
-
-            output.WriteLine("START Prepare");
-            var stopwatch = new Stopwatch();
-            options = method.Prepare(options) as BruteForceOptions;
-            stopwatch.Stop();
-            string elapsedPrepare = stopwatch.Elapsed.ToString("G");
-            output.WriteLine("END Prepare");
-
-            for (int i = 0; i < numOfTests + 1; i++)
-            {
-                stopwatch = new Stopwatch();
-                var (jobOrder, minTardiness) = method.Solve(options, stopwatch);
-                stopwatch.Stop();
-                var result = new Result()
-                {
-                    Score = minTardiness,
-                    ExecElapsed = stopwatch.Elapsed.ToString("G"),
-                    PrepareElapsed = elapsedPrepare,
-                };
-                if (i != 0) results.Add(result);
-                Helper.LogStatus($"{method.GetType().Name} {i}/{numOfTests} | ExecElapsed = {result.ExecElapsed}");
-            }
-
-            var min = results.Min(x => x.Score);
-            if (bestTardiness > min) { bestTardiness = min; Loader.TrySaveNewBest(file, bestTardiness, Helper.AppSettings.SolvedDictFileName); }
-            Helper.WriteResultsToCsv(method.GetType().Name, jobsCount, bestTardiness, results);
-        }
+        public void BruteForceTest() =>
+            TestMethod(new BruteForceMethod(), new BruteForceOptions() { Data = data, }); 
 
         [Fact]
-        public void DynamicProgrammingTest()
-        {
-            var results = new List<Result>();
-            var options = new DynamicProgrammingOptions() { Data = data, };
-            var method = new DynamicProgrammingMethod();
-
-            output.WriteLine("START Prepare");
-            var stopwatch = new Stopwatch();
-            options = method.Prepare(options) as DynamicProgrammingOptions;
-            stopwatch.Stop();
-            string elapsedPrepare = stopwatch.Elapsed.ToString("G");
-            output.WriteLine("END Prepare");
-
-            for (int i = 0; i < numOfTests + 1; i++)
-            {
-                stopwatch = new Stopwatch();
-                var (jobOrder, minTardiness) = method.Solve(options, stopwatch);
-                stopwatch.Stop();
-                var result = new Result()
-                {
-                    Score = minTardiness,
-                    ExecElapsed = stopwatch.Elapsed.ToString("G"),
-                    PrepareElapsed = elapsedPrepare,
-                };
-                if (i != 0) results.Add(result);
-                Helper.LogStatus($"{method.GetType().Name} {i}/{numOfTests} | ExecElapsed = {result.ExecElapsed}");
-            }
-
-            var min = results.Min(x => x.Score);
-            if (bestTardiness > min) { bestTardiness = min; Loader.TrySaveNewBest(file, bestTardiness, Helper.AppSettings.SolvedDictFileName); }
-            Helper.WriteResultsToCsv(method.GetType().Name, jobsCount, bestTardiness, results);
-        }
+        public void DynamicProgrammingTest()  =>
+            TestMethod(new DynamicProgrammingMethod(), new DynamicProgrammingOptions() { Data = data, });
 
         [Fact]
-        public void GeneticAlgorithmTest()
-        {
-            var results = new List<Result>();
-            var options = new GeneticAlgorithmOptions()
+        public void GeneticAlgorithmTest() =>
+            TestMethod(new GeneticAlgorithmMethod(), new GeneticAlgorithmOptions()
             {
                 Data = data,
                 ChromosomeCount = 100,
@@ -122,36 +26,6 @@ namespace XUnitTestProject
                 MutationRate = 0.8,
                 NumberOfIterations = 1000,
                 SelectionMechanism = Solver.Utils.SelectionMechanismEnum.RouletteWheel,
-            };
-            var method = new GeneticAlgorithmMethod();
-
-            output.WriteLine("START Prepare");
-            var stopwatch = new Stopwatch();
-            options = method.Prepare(options) as GeneticAlgorithmOptions;
-            stopwatch.Stop();
-            string elapsedPrepare = stopwatch.Elapsed.ToString("G");
-            output.WriteLine("END Prepare");
-
-            for (int i = 0; i < numOfTests + 1; i++)
-            {
-                stopwatch = new Stopwatch();
-                var (jobOrder, minTardiness) = method.Solve(options, stopwatch);
-                stopwatch.Stop();
-                var result = new Result()
-                {
-                    Score = minTardiness,
-                    ExecElapsed = stopwatch.Elapsed.ToString("G"),
-                    PrepareElapsed = elapsedPrepare,
-                };
-
-                if (i != 0) results.Add(result);
-                Helper.LogStatus($"{method.GetType().Name} {i}/{numOfTests} | ExecElapsed = {result.ExecElapsed}");
-            }
-
-            var min = results.Min(x => x.Score);
-            if (bestTardiness > min) { bestTardiness = min; Loader.TrySaveNewBest(file, bestTardiness, Helper.AppSettings.SolvedDictFileName); }
-            string expandedName = $"_CC{options.ChromosomeCount}_CR{options.CrossoverRate:0%}_MR{options.MutationRate:0%}_I{options.NumberOfIterations.ToString()}_{string.Join("", options.SelectionMechanism.ToString().Where(x => char.IsUpper(x)))}";
-            Helper.WriteResultsToCsv(method.GetType().Name, jobsCount, bestTardiness, results, expandedName);
-        }
+            });
     }
 }
